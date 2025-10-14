a4e19e7fe475bb41705797dd1cd1a66413d21382
Oleh Zimin <ziminoleh@gmail.com>
2025-10-14 18:51:21 +0200

    Initial Commit

---
+++ ConcurrencyPlayground/ConcurrencyPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyPlaygroundApp.swift
+//  ConcurrencyPlayground
+//
+//  Created by Oleh Zimin on 14.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
